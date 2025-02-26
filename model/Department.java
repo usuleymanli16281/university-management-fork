0d4871a5f664c5151c31e2a26baba3c39aaac8fc
usuleymanli16281 <usuleymanli16281@ada.edu.az>
2025-02-26 19:25:18 +0400

    Modified Department class

--- model/Department.java
+++ model/Department.java
@@ -1,5 +1,57 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.person.Employee;
+
 public class Department {
-    
+    private int id;
+    private String name;
+    private List<Employee> employees;
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        this.employees.add(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        this.employees.remove(employee);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Department{")
+          .append("id=").append(id)
+          .append(", name='").append(name).append('\'')
+          .append(", employees=").append(employees)
+          .append('}');
+        return sb.toString();
+    }
 }
